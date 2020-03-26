ff0bbf56d4e90f0dbe31d9186424eede3a7ba3c4
guiselair <guilherme.lima1997@hotmail.com>
2020-03-26 20:26:12 -0300

    Frontend Parcial

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { FiLogIn } from "react-icons/fi"
+
+import "./styles.css"
+import heroesImage from "../../assets/heroes.png"
+import logoImg from "../../assets/logo.svg"
+
+export default function Logon(){
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="LogoHeroes"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input type="text" placeholder="Sua ID"/>
+                    <button className="button"  type="submit">Entrar</button>
+                    <Link to="/register" className="back-link">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={heroesImage} alt="Heroes"/>
+        </div>
+    )
+}
